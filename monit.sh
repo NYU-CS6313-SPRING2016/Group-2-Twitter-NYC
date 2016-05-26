5eecc10d0b8f0928efbb5d0f35b530bfc083da32
EC2 Default User <ec2-user@ip-172-31-21-65.us-west-2.compute.internal>
2016-05-26 20:20:57 +0000

    new style

--- monit.sh
+++ monit.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 pid=`ps -ef|grep "node"|grep "app.js" |grep -v grep |wc -l`
 if [ $pid -eq 0 ] ; then 
+	sudo killall -9 node
 	nohup sudo node --expose-gc /home/ec2-user/twitter_nyc_private/app.js >/dev/null 2>&1 &
 fi
